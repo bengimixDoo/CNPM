3fb78691048236af0a2abdf11be9640747d45f8f
alpha0035 <alpha07eg@gmail.com>
2025-12-22 21:48:13 +0700

    using swagger to write API document

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -5,6 +5,15 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from residents.urls import resident_router, apartment_router, history_router
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(title="API", default_version='v1'),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +26,10 @@ urlpatterns = [
     path('api/residents/', include(resident_router.urls)),
     path('api/apartments/', include(apartment_router.urls)),
     path('api/history/', include(history_router.urls)),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
