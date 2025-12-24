d6893ea9f447e78c57e6388063d36f9a648abce8
nhat-nk <contact.nhat.nguyenkhac@gmail.com>
2025-12-25 04:50:52 +0700

    Update project-level urls

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
 )
+
 # Import routers của residents (Giữ nguyên phần cũ của bạn)
 from residents.urls import resident_router, apartment_router, history_router
 
@@ -12,19 +13,25 @@ from residents.urls import resident_router, apartment_router, history_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
 
     # 1. Users & Auth
-    path('api/users/', include('users.urls')),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('users.urls')),
 
     # 2. Residents (Giữ nguyên cấu trúc cũ bạn đang chạy)
     path('api/residents/', include(resident_router.urls)),
     path('api/apartments/', include(apartment_router.urls)),
     path('api/history/', include(history_router.urls)),
 
+    # Finance App
+    path('api/', include('finance.urls')),
+
     # 3. Services [SỬA LẠI CHỖ NÀY]
     # Dùng include('services.urls') để gom tất cả (pricing, utilities, vehicles...)
     # vào đường dẫn /api/services/...
     path('api/services/', include('services.urls')),
+
+    # Dashboard & Cross-Cutting APIs
+    path('api/', include('dashboard.urls')),
 ]
